963af9f6d6fd7064005a5be5b3b5f14b40e93b77
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-10-06 04:53:44 +0200

    feat: +2 tasks completed

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -204,8 +204,16 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n !== 2 && n % 2 === 0) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0 && i !== n) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -223,8 +231,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return typeof Number(value) === 'number' || def;
 }
 
 module.exports = {
